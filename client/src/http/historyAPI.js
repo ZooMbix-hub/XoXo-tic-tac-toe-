356acbbcb8a9679e26a494f78d911eb70d3ac7fe
ZooMbix <84900507+ZooMbix-hub@users.noreply.github.com>
2022-07-16 17:14:45 +0500

    Delete client directory

--- client/src/http/historyAPI.js
+++
@@ -1,9 +0,0 @@
-import {$host} from './index';
-
-export const getHistory = async (page, limit = 6) => {
-    const {data} = await $host.get('/history', {params: {
-        page,
-        limit
-    }});
-    return data;
-}
--- client/src/http/ratingAPI.js
+++
@@ -1,6 +0,0 @@
-import {$host} from './index';
-
-export const getAll = async () => {
-    const {data} = await $host.get('/rating/all');
-    return data;
-}
